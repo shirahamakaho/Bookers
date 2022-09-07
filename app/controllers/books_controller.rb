e91db09d5e44d5e0b7fba701c1538bc6ba15ae93
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-07 09:55:51 +0000

    break

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
-  def new
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+     @book = Book.new
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
